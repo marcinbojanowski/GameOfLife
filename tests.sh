96b124889ae6d463a632fb18f23614f796b7d5b7
Marcin Bojanowski <marcin.bojanowski@made.com>
2016-11-06 13:39:36 +0000

    Clean tests.sh script

--- tests.sh
+++ tests.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-# run phpunit in /var/www dir 
-# docker exec -it gameoflife sh -c 'cd /var/www/;  phpunit --debug --'
-
-# todo: unit tests bootstraping and execute it like:
-
 docker exec -it gameoflife phpunit -c /var/www/tests/unit/phpunit.xml.dist
